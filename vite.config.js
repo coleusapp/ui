a7a06b4f4ff4dcfabd19ef5ff0cb8054ef127e4c
Amirmasoud <amirmasood33@gmail.com>
2023-11-18 10:06:18 -0700

    wip

---
+++ vite.config.js
@@ -0,0 +1,36 @@
+import {defineConfig} from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+
+const rootPath = '../../../../coleus-dev';
+
+export default defineConfig({
+    envDir: rootPath,
+    resolve: {
+        alias: {
+            '@account': path.resolve(__dirname, './resources/js'),
+        },
+    },
+    build: {
+        modulePreload: false,
+        assetsDir: '.'
+    },
+    plugins: [
+        laravel({
+            input: "resources/js/app.js",
+            refresh: true,
+            buildDirectory: 'dist',
+            publicDirectory: 'resources',
+            hotFile: `${rootPath}/public/hot`
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+    ],
+});
