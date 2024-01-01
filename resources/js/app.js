44c0e8b99cc3ef488309545e85fd9e561f4a21e7
Amirmasoud <amirmasood33@gmail.com>
2024-01-01 16:39:19 -0700

    primevue

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,8 @@ import {createApp, h} from 'vue';
 import {createInertiaApp, Link} from '@inertiajs/vue3';
 import ThePlain from "@ui/Layouts/ThePlain.vue";
 import {resolvePageComponent} from "laravel-vite-plugin/inertia-helpers";
+import PrimeVue from 'primevue/config';
+import Lara from '../presets/lara'
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Account';
 
@@ -16,6 +18,10 @@ createInertiaApp({
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
+            .use(PrimeVue, {
+                unstyled: true,
+                pt: Lara
+            })
             .mount(el)
     },
     progress: {
